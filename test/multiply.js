80bc82751e7fc36b3b2f70579a9b98fb0a8c4cd2
ajlopez <webmaster@ajlopez.com>
2017-07-18 12:08:22 -0300

    Multiply vector by vector

--- test/multiply.js
+++ test/multiply.js
@@ -64,3 +64,24 @@ exports['multiply vector by number'] = function (test) {
 	test.deepEqual(result.shape(), [ 4 ]);
 	test.deepEqual(result.value(), [ 2, 4, 6, 42 ]);
 };
+
+exports['multiply vector by vector'] = function (test) {
+	var value1 = st.constant([ 1, 2, 3 ]);
+	var value2 = st.constant([ 2, 3, 4 ]);
+	
+	var oper = st.multiply(value1, value2);
+	
+	test.ok(oper.isNumber());
+	test.ok(!oper.isVector());
+	test.ok(!oper.isMatrix());
+	test.deepEqual(oper.shape(), []);
+	test.equal(oper.value(), 20);
+	
+	var result = oper.evaluate();
+		
+	test.ok(oper.isNumber());
+	test.ok(!oper.isVector());
+	test.ok(!oper.isMatrix());
+	test.deepEqual(oper.shape(), []);
+	test.equal(oper.value(), 20);
+};
