9b069c5eb8dc2094d5d12f0661ec4e6964bc2d3d
ajlopez <webmaster@ajlopez.com>
2017-07-19 12:04:13 -0300

    Add numbers

---
+++ test/add.js
@@ -0,0 +1,25 @@
+
+var st = require('..');
+
+exports['add numbers'] = function (test) {
+	var number1 = st.constant(20);
+	var number2 = st.constant(22);
+	
+	var oper = st.add(number1, number2);
+	
+	test.ok(oper.isNumber());
+	test.ok(!oper.isVector());
+	test.ok(!oper.isMatrix());
+	test.deepEqual(oper.shape(), []);
+	
+	test.equal(oper.value(), 42);
+	
+	var result = oper.evaluate();
+	
+	test.ok(result.isNumber());
+	test.ok(!result.isVector());
+	test.ok(!result.isMatrix());
+	test.deepEqual(result.shape(), []);
+	test.equal(result.value(), 42);
+};
+
