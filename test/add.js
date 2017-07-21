1cd989185e2f3a7113bb3c4542085ab0f6f7e689
ajlopez <webmaster@ajlopez.com>
2017-07-21 10:21:10 -0300

    Adding matrices, WIP

--- test/add.js
+++ test/add.js
@@ -23,7 +23,7 @@ exports['add numbers'] = function (test) {
 	test.equal(result.value(), 42);
 };
 
-exports['add vector to number'] = function (test) {
+exports['add vector to vector'] = function (test) {
 	var value1 = st.constant([ 1, 2, 3 ]);
 	var value2 = st.constant([ 2, 3, 4 ]);
 	
@@ -42,4 +42,25 @@ exports['add vector to number'] = function (test) {
 	test.ok(!result.isMatrix());
 	test.deepEqual(result.shape(), [ 3 ]);
 	test.deepEqual(result.value(), [ 3, 5, 7 ]);
+};
+
+exports['add matrix to matrix'] = function (test) {
+	var value1 = st.constant([ [ 1, 2, 3 ], [ 3, 4, 5 ] ]);
+	var value2 = st.constant([ [ 2, 3, 4 ], [ 10, 11, 12 ] ]);
+	
+	var oper = st.add(value1, value2);
+	
+	test.ok(!oper.isNumber());
+	test.ok(!oper.isVector());
+	test.ok(oper.isMatrix());
+	test.deepEqual(oper.shape(), [ 2, 3 ]);
+	test.deepEqual(oper.value(), [ [ 3, 5, 7 ], [ 13, 14, 17 ] ]);
+	
+	var result = oper.evaluate();
+	
+	test.ok(!result.isNumber());
+	test.ok(!result.isVector());
+	test.ok(result.isMatrix());
+	test.deepEqual(result.shape(), [ 2, 3 ]);
+	test.deepEqual(result.value(), [ [ 3, 5, 7 ], [ 13, 14, 17 ] ]);
 };
