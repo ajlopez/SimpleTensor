165b1f1faff28745aa704a9629aaef2a5507516a
ajlopez <webmaster@ajlopez.com>
2017-08-13 14:21:59 -0300

    Session run placeholder with constant

--- test/session.js
+++ test/session.js
@@ -8,3 +8,9 @@ exports['session run constant'] = function (test) {
 	test.equal(sess.run(x), 42);
 };
 
+exports['session run placeholder using context'] = function (test) {
+	var sess = st.session();
+	var x = st.placeholder('x', []);
+	
+	test.equal(sess.run(x, { context: { x: 42 } }), 42);
+};
