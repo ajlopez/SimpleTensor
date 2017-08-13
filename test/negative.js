1be8392c1b2b820f410c4fe99d3dacd03dff6fc5
ajlopez <webmaster@ajlopez.com>
2017-08-13 18:30:09 -0300

    Negative number

---
+++ test/negative.js
@@ -0,0 +1,23 @@
+
+var st = require('..');
+
+exports['negative number'] = function (test) {
+	var value = st.constant(42);
+	
+	var oper = st.negative(value);
+	
+	test.ok(oper.isNumber());
+	test.ok(!oper.isVector());
+	test.ok(!oper.isMatrix());
+	test.deepEqual(oper.shape(), []);
+	
+	test.equal(oper.value(), -42);
+	
+	var result = oper.evaluate();
+	
+	test.ok(result.isNumber());
+	test.ok(!result.isVector());
+	test.ok(!result.isMatrix());
+	test.deepEqual(result.shape(), []);
+	test.equal(result.value(), -42);
+};
