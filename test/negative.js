38ef5f8349f02a0b81ee6016aec7ab46a9a5a71b
ajlopez <webmaster@ajlopez.com>
2017-08-13 18:43:12 -0300

    Negative matrix

--- test/negative.js
+++ test/negative.js
@@ -42,3 +42,25 @@ exports['negative vector'] = function (test) {
 	test.deepEqual(result.shape(), [ 3 ]);
 	test.deepEqual(result.value(), [ -1, -2, -3 ]);
 };
+
+exports['negative matrix'] = function (test) {
+	var value = st.constant([ [ 1, 2, 3 ], [ 4, 5, 6 ] ]);
+	
+	var oper = st.negative(value);
+	
+	test.ok(!oper.isNumber());
+	test.ok(!oper.isVector());
+	test.ok(oper.isMatrix());
+	test.deepEqual(oper.shape(), [ 2, 3 ]);
+	
+	test.deepEqual(oper.value(), [ [ -1, -2, -3 ], [ -4, -5, -6 ] ]);
+	
+	var result = oper.evaluate();
+	
+	test.ok(!result.isNumber());
+	test.ok(!result.isVector());
+	test.ok(result.isMatrix());
+	test.deepEqual(result.shape(), [ 2, 3 ]);
+	test.deepEqual(result.value(), [ [ -1, -2, -3 ], [ -4, -5, -6 ] ]);
+};
+
