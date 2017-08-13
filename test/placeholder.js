0fa10a60d547867ef6ce591b716de5b6a12516f6
ajlopez <webmaster@ajlopez.com>
2017-08-13 14:07:33 -0300

    Evaluate vector in placeholder

--- test/placeholder.js
+++ test/placeholder.js
@@ -1,7 +1,7 @@
 
 var st = require('..');
 
-exports['evaluate placeholder'] = function (test) {
+exports['evaluate number in placeholder'] = function (test) {
 	var result = st.placeholder('x', []);
 	
 	test.ok(result);
@@ -13,5 +13,21 @@ exports['evaluate placeholder'] = function (test) {
 	test.ok(!result.isMatrix());
 	
 	test.equal(result.evaluate({ x: 42 }).value(), 42);
+	test.equal(result.evaluate({ y: 42 }), null);
+};
+
+exports['evaluate vector in placeholder'] = function (test) {
+	var result = st.placeholder('x', [ 3 ]);
+	
+	test.ok(result);
+	test.equal(result.rank(), 1);
+	test.deepEqual(result.shape(), [ 3 ]);
+	test.equal(result.value(), null);
+
+	test.ok(!result.isNumber());
+	test.ok(result.isVector());
+	test.ok(!result.isMatrix());
+	
+	test.deepEqual(result.evaluate({ x: [ 1, 2, 3 ] }).value(), [ 1, 2, 3 ]);
 	test.equal(result.evaluate({ y: 42 }), null);
 };
