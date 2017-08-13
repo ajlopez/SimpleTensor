cb44036e0b655384c99217dc70c8704acc7cc29a
ajlopez <webmaster@ajlopez.com>
2017-08-13 18:12:57 -0300

    Pow vectors

--- test/pow.js
+++ test/pow.js
@@ -22,3 +22,25 @@ exports['pow numbers'] = function (test) {
 	test.deepEqual(result.shape(), []);
 	test.equal(result.value(), 144);
 };
+
+exports['pow vectors'] = function (test) {
+	var vector1 = st.constant([ 2, 3, 4 ]);
+	var vector2 = st.constant([ 2, 3, 1 ]);
+	
+	var oper = st.pow(vector1, vector2);
+	
+	test.ok(!oper.isNumber());
+	test.ok(oper.isVector());
+	test.ok(!oper.isMatrix());
+	test.deepEqual(oper.shape(), [ 3 ]);
+	
+	test.deepEqual(oper.value(), [ 4, 27, 4 ]);
+	
+	var result = oper.evaluate();
+	
+	test.ok(!result.isNumber());
+	test.ok(result.isVector());
+	test.ok(!result.isMatrix());
+	test.deepEqual(result.shape(), [ 3 ]);
+	test.deepEqual(result.value(), [ 4, 27, 4 ]);
+};
