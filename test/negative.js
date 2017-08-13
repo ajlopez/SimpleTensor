5d3f76176c238991c59e9d52c1793fc1c5bec620
ajlopez <webmaster@ajlopez.com>
2017-08-13 18:35:30 -0300

    Negative vector

--- test/negative.js
+++ test/negative.js
@@ -21,3 +21,24 @@ exports['negative number'] = function (test) {
 	test.deepEqual(result.shape(), []);
 	test.equal(result.value(), -42);
 };
+
+exports['negative vector'] = function (test) {
+	var value = st.constant([ 1, 2, 3 ]);
+	
+	var oper = st.negative(value);
+	
+	test.ok(!oper.isNumber());
+	test.ok(oper.isVector());
+	test.ok(!oper.isMatrix());
+	test.deepEqual(oper.shape(), [ 3 ]);
+	
+	test.deepEqual(oper.value(), [ -1, -2, -3 ]);
+	
+	var result = oper.evaluate();
+	
+	test.ok(!result.isNumber());
+	test.ok(result.isVector());
+	test.ok(!result.isMatrix());
+	test.deepEqual(result.shape(), [ 3 ]);
+	test.deepEqual(result.value(), [ -1, -2, -3 ]);
+};
