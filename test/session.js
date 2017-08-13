d89b0ee73548eb62aa27ca025a69b2746e3217ee
ajlopez <webmaster@ajlopez.com>
2017-08-13 14:26:12 -0300

    Session run add two vectors in placeholders

--- test/session.js
+++ test/session.js
@@ -14,3 +14,20 @@ exports['session run placeholder using context'] = function (test) {
 	
 	test.equal(sess.run(x, { context: { x: 42 } }), 42);
 };
+
+exports['session run add operation with placeholders using context'] = function (test) {
+	var sess = st.session();
+	var x = st.placeholder('x', []);
+	var add = st.add(x, x);
+	
+	test.equal(sess.run(add, { context: { x: 21 } }), 42);
+};
+
+exports['session run add operation with vectors in placeholders'] = function (test) {
+	var sess = st.session();
+	var x = st.placeholder('x', [ 3 ]);
+	var y = st.placeholder('y', [ 3 ]);
+	var add = st.add(x, y);
+	
+	test.deepEqual(sess.run(add, { context: { x: [ 1, 2, 3 ], y: [ 4, 5, 6 ]} }), [ 5, 7, 9 ]);
+};
