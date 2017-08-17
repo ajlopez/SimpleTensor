dbfacdf798184461bbb1b4a743ad56a30caaf46a
ajlopez <webmaster@ajlopez.com>
2017-08-17 10:18:57 -0300

    Ceil operation on vectors, matrices

--- test/ceil.js
+++ test/ceil.js
@@ -22,3 +22,45 @@ exports['ceil number'] = function (test) {
 	test.equal(result.value(), 42);
 };
 
+exports['ceil vector'] = function (test) {
+	var value = st.constant([ 1.5, 2.6, 3.7 ]);
+	
+	var oper = st.ceil(value);
+	
+	test.ok(!oper.isNumber());
+	test.ok(oper.isVector());
+	test.ok(!oper.isMatrix());
+	test.deepEqual(oper.shape(), [ 3 ]);
+	
+	test.deepEqual(oper.value(), [ 2, 3, 4 ]);
+	
+	var result = oper.evaluate();
+	
+	test.ok(!result.isNumber());
+	test.ok(result.isVector());
+	test.ok(!result.isMatrix());
+	test.deepEqual(result.shape(), [ 3 ]);
+	test.deepEqual(result.value(), [ 2, 3, 4 ]);
+};
+
+exports['ceil matrix'] = function (test) {
+	var value = st.constant([ [ 1.5, 2.6, 3.7 ], [ 1, 2, 3 ] ]);
+	
+	var oper = st.ceil(value);
+	
+	test.ok(!oper.isNumber());
+	test.ok(!oper.isVector());
+	test.ok(oper.isMatrix());
+	test.deepEqual(oper.shape(), [ 2, 3 ]);
+	
+	test.deepEqual(oper.value(), [ [ 2, 3, 4 ], [ 1, 2, 3 ] ]);
+	
+	var result = oper.evaluate();
+	
+	test.ok(!result.isNumber());
+	test.ok(!result.isVector());
+	test.ok(result.isMatrix());
+	test.deepEqual(result.shape(), [ 2, 3 ]);
+	test.deepEqual(result.value(), [ [ 2, 3, 4 ], [ 1, 2, 3 ] ]);
+};
+
