5085e631f1ce77ab7459698139a614feca886011
ajlopez <webmaster@ajlopez.com>
2017-08-22 17:56:05 -0300

    Identity operation

---
+++ test/identity.js
@@ -0,0 +1,66 @@
+
+var st = require('..');
+
+exports['identity number'] = function (test) {
+	var number = st.constant(42);
+	
+	var oper = st.identity(number);
+	
+	test.ok(oper.isNumber());
+	test.ok(!oper.isVector());
+	test.ok(!oper.isMatrix());
+	test.deepEqual(oper.shape(), []);
+	
+	test.equal(oper.value(), 42);
+	
+	var result = oper.evaluate();
+	
+	test.ok(result.isNumber());
+	test.ok(!result.isVector());
+	test.ok(!result.isMatrix());
+	test.deepEqual(result.shape(), []);
+	test.equal(result.value(), 42);
+};
+
+exports['identity vector'] = function (test) {
+	var value = st.constant([ 1, 2, 3 ]);
+	
+	var oper = st.identity(value);
+	
+	test.ok(!oper.isNumber());
+	test.ok(oper.isVector());
+	test.ok(!oper.isMatrix());
+	test.deepEqual(oper.shape(), [ 3 ]);
+	
+	test.deepEqual(oper.value(), [ 1, 2, 3 ]);
+	
+	var result = oper.evaluate();
+	
+	test.ok(!result.isNumber());
+	test.ok(result.isVector());
+	test.ok(!result.isMatrix());
+	test.deepEqual(result.shape(), [ 3 ]);
+	test.deepEqual(result.value(), [ 1, 2, 3 ]);
+};
+
+exports['identity matrix'] = function (test) {
+	var value = st.constant([ [ 1, 2, 3 ], [ 1, 2, 3 ] ]);
+	
+	var oper = st.identity(value);
+	
+	test.ok(!oper.isNumber());
+	test.ok(!oper.isVector());
+	test.ok(oper.isMatrix());
+	test.deepEqual(oper.shape(), [ 2, 3 ]);
+	
+	test.deepEqual(oper.value(), [ [ 1, 2, 3 ], [ 1, 2, 3 ] ]);
+	
+	var result = oper.evaluate();
+	
+	test.ok(!result.isNumber());
+	test.ok(!result.isVector());
+	test.ok(result.isMatrix());
+	test.deepEqual(result.shape(), [ 2, 3 ]);
+	test.deepEqual(result.value(), [ [ 1, 2, 3 ], [ 1, 2, 3 ] ]);
+};
+
