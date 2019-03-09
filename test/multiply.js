feeadff7b53b5ccd0fb524837fea87a896f8e1ed
ajlopez <webmaster@ajlopez.com>
2019-03-09 13:47:41 -0300

    Refactor multiply operation

--- test/multiply.js
+++ test/multiply.js
@@ -71,38 +71,39 @@ exports['multiply vector by vector'] = function (test) {
 	
 	var oper = st.multiply(value1, value2);
 	
-	test.ok(oper.isNumber());
-	test.ok(!oper.isVector());
+	test.ok(!oper.isNumber());
+	test.ok(oper.isVector());
 	test.ok(!oper.isMatrix());
-	test.deepEqual(oper.shape(), []);
-	test.equal(oper.value(), 20);
+	test.deepEqual(oper.shape(), [ 3 ]);
+	test.deepEqual(oper.value(), [ 2, 6, 12 ]);
 	
 	var result = oper.evaluate();
 		
-	test.ok(oper.isNumber());
-	test.ok(!oper.isVector());
+	test.ok(!oper.isNumber());
+	test.ok(oper.isVector());
 	test.ok(!oper.isMatrix());
-	test.deepEqual(oper.shape(), []);
-	test.equal(oper.value(), 20);
+	test.deepEqual(oper.shape(), [ 3 ]);
+	test.deepEqual(oper.value(), [ 2, 6, 12 ]);
 };
 
 exports['multiply matrix by matrix'] = function (test) {
 	var value1 = st.constant([ [ 1, 2, 3 ], [ 4, 5, 6 ] ]);
-	var value2 = st.constant([ [ 7 ], [ 8 ], [ 9 ] ]);
+	var value2 = st.constant([ [ 7, 8, 9 ], [ 10, 11, 12 ] ]);
 	
 	var oper = st.multiply(value1, value2);
 	
 	test.ok(!oper.isNumber());
 	test.ok(!oper.isVector());
 	test.ok(oper.isMatrix());
-	test.deepEqual(oper.shape(), [ 2 , 1 ]);
-	test.deepEqual(oper.value(), [ [ 50 ], [ 122 ] ]);
+	test.deepEqual(oper.shape(), [ 2 , 3 ]);
+	test.deepEqual(oper.value(), [ [ 7, 16, 27 ], [ 40, 55, 72 ] ]);
 	
 	var result = oper.evaluate();
 		
 	test.ok(!oper.isNumber());
 	test.ok(!oper.isVector());
 	test.ok(oper.isMatrix());
-	test.deepEqual(oper.shape(), [ 2 , 1 ]);
-	test.deepEqual(oper.value(), [ [ 50 ], [ 122 ] ]);
+	test.deepEqual(oper.shape(), [ 2 , 3 ]);
+	test.deepEqual(oper.value(), [ [ 7, 16, 27 ], [ 40, 55, 72 ] ]);
 };
+
