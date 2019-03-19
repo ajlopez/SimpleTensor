1b640b5be48a471e57d26c7eb5e4c631640d2f8b
ajlopez <webmaster@ajlopez.com>
2019-03-19 19:30:43 -0300

    First functions in math package

---
+++ test/math.js
@@ -0,0 +1,12 @@
+
+var st = require('..');
+
+exports['math package'] = function (test) {
+    test.ok(st.math);
+    test.equal(typeof st.math, 'object');
+    test.equal(st.math.abs, st.abs);
+    test.equal(st.math.cos, st.cos);
+    test.equal(st.math.sin, st.sin);
+    test.equal(st.math.acos, st.acos);
+    test.equal(st.math.asin, st.asin);
+};
