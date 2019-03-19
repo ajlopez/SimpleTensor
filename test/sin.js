eade52475c548cb5ead2e59d5874247b8217390b
ajlopez <webmaster@ajlopez.com>
2019-03-19 19:14:54 -0300

    Sin operation

--- test/sin.js
+++ test/sin.js
@@ -1,17 +1,17 @@
 
 var st = require('..');
 
-exports['cos number'] = function (test) {
+exports['sin number'] = function (test) {
 	var number = st.constant(1.5);
 	
-	var oper = st.cos(number);
+	var oper = st.sin(number);
 	
 	test.ok(oper.isNumber());
 	test.ok(!oper.isVector());
 	test.ok(!oper.isMatrix());
 	test.deepEqual(oper.shape(), []);
 	
-	test.equal(oper.value(), Math.cos(1.5));
+	test.equal(oper.value(), Math.sin(1.5));
 	
 	var result = oper.evaluate();
 	
@@ -19,20 +19,20 @@ exports['cos number'] = function (test) {
 	test.ok(!result.isVector());
 	test.ok(!result.isMatrix());
 	test.deepEqual(result.shape(), []);
-	test.equal(result.value(), Math.cos(1.5));
+	test.equal(result.value(), Math.sin(1.5));
 };
 
-exports['cos vector'] = function (test) {
+exports['sin vector'] = function (test) {
 	var value = st.constant([ -1, 2.5, -0.3 ]);
 	
-	var oper = st.cos(value);
+	var oper = st.sin(value);
 	
 	test.ok(!oper.isNumber());
 	test.ok(oper.isVector());
 	test.ok(!oper.isMatrix());
 	test.deepEqual(oper.shape(), [ 3 ]);
 	
-	test.deepEqual(oper.value(), [ Math.cos(-1), Math.cos(2.5), Math.cos(-0.3) ]);
+	test.deepEqual(oper.value(), [ Math.sin(-1), Math.sin(2.5), Math.sin(-0.3) ]);
 	
 	var result = oper.evaluate();
 	
@@ -40,20 +40,20 @@ exports['cos vector'] = function (test) {
 	test.ok(result.isVector());
 	test.ok(!result.isMatrix());
 	test.deepEqual(result.shape(), [ 3 ]);
-	test.deepEqual(result.value(), [ Math.cos(-1), Math.cos(2.5), Math.cos(-0.3) ]);
+	test.deepEqual(result.value(), [ Math.sin(-1), Math.sin(2.5), Math.sin(-0.3) ]);
 };
 
-exports['cos matrix'] = function (test) {
+exports['sin matrix'] = function (test) {
 	var value = st.constant([ [ 3.5, -0.2, 1 ], [ 1, -0.2, 0.3 ] ]);
 	
-	var oper = st.cos(value);
+	var oper = st.sin(value);
 	
 	test.ok(!oper.isNumber());
 	test.ok(!oper.isVector());
 	test.ok(oper.isMatrix());
 	test.deepEqual(oper.shape(), [ 2, 3 ]);
 	
-	test.deepEqual(oper.value(), [ [ Math.cos(3.5), Math.cos(-0.2), Math.cos(1) ], [ Math.cos(1), Math.cos(-0.2), Math.cos(0.3) ] ]);
+	test.deepEqual(oper.value(), [ [ Math.sin(3.5), Math.sin(-0.2), Math.sin(1) ], [ Math.sin(1), Math.sin(-0.2), Math.sin(0.3) ] ]);
 	
 	var result = oper.evaluate();
 	
@@ -61,6 +61,6 @@ exports['cos matrix'] = function (test) {
 	test.ok(!result.isVector());
 	test.ok(result.isMatrix());
 	test.deepEqual(result.shape(), [ 2, 3 ]);
-	test.deepEqual(result.value(), [ [ Math.cos(3.5), Math.cos(-0.2), Math.cos(1) ], [ Math.cos(1), Math.cos(-0.2), Math.cos(0.3) ] ]);
+	test.deepEqual(result.value(), [ [ Math.sin(3.5), Math.sin(-0.2), Math.sin(1) ], [ Math.sin(1), Math.sin(-0.2), Math.sin(0.3) ] ]);
 };
 
