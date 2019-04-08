8a1994917a2ddc188834effb3c9fdc208fe436b6
ajlopez <webmaster@ajlopez.com>
2019-04-08 10:37:59 -0300

    Cosh, sinh operations in math module

--- test/math.js
+++ test/math.js
@@ -5,10 +5,15 @@ exports['math package'] = function (test) {
     test.ok(st.math);
     test.equal(typeof st.math, 'object');
     test.equal(st.math.abs, st.abs);
+    
     test.equal(st.math.cos, st.cos);
     test.equal(st.math.sin, st.sin);
     test.equal(st.math.tan, st.tan);
+    
     test.equal(st.math.acos, st.acos);
     test.equal(st.math.asin, st.asin);
     test.equal(st.math.atan, st.atan);
+    
+    test.equal(st.math.cosh, st.cosh);
+    test.equal(st.math.sinh, st.sinh);
 };
